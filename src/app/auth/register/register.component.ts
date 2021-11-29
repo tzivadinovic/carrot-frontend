cb605ef8635f9d5f358dcfac03c3702a8a561568
tzivadinovic <zivadinovictomislav@gmail.com>
2021-11-30 00:21:17 +0100

    created register page v2

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form = new FormGroup({
+    firstname: new FormControl('', null),
+    lastname: new FormControl('', null),
+    email: new FormControl('', null),
+    username: new FormControl('', null),
+    password: new FormControl('', null),
+    repeatedPassword: new FormControl('', null)
+  });
+
+  passwordShown: boolean = false;
+
+  showHidePassword(): void {
+    this.passwordShown = !this.passwordShown;
+  }
 
   constructor() { }
 
